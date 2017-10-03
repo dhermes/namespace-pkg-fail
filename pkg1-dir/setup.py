0918c81987de59bc60d22e2dc3421c81d9e1329a
Danny Hermes <daniel.j.hermes@gmail.com>
2017-10-03 14:21:59 -0700

    Making more generic.

--- pkg1-dir/setup.py
+++ pkg1-dir/setup.py
@@ -1,15 +1,18 @@
 import setuptools
 
 
+NAME = 'pkg1'
+
+
 def main():
     setuptools.setup(
-        name='pkg1',
+        name=NAME,
         version='0.0.1',
         description='Some text',
         long_description='README',
         install_requires=[],
-        namespace_packages=['pkg1'],
-        packages=['pkg1'],
+        namespace_packages=[NAME],
+        packages=[NAME],
         package_dir={'': 'src'},
     )
 
--- pkg2-dir/setup.py
+++ pkg2-dir/setup.py
@@ -1,15 +1,18 @@
 import setuptools
 
 
+NAME = 'pkg2'
+
+
 def main():
     setuptools.setup(
-        name='pkg2',
+        name=NAME,
         version='0.0.1',
         description='Some text',
         long_description='README',
         install_requires=[],
-        namespace_packages=['pkg2'],
-        packages=['pkg2'],
+        namespace_packages=[NAME],
+        packages=[NAME],
         package_dir={'': 'src'},
     )
 
